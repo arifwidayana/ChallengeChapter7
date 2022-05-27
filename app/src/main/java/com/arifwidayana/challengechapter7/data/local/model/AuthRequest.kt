80df189d695522de7dfdc792f84e368fdebab518
Arif Widayana <arifwidayana@gmail.com>
2022-05-27 08:59:55 -0700

    login and register user with getting data from room database

---
+++ app/src/main/java/com/arifwidayana/challengechapter7/data/local/model/AuthRequest.kt
@@ -0,0 +1,6 @@
+package com.arifwidayana.challengechapter7.data.local.model
+
+data class AuthRequest(
+    var username: String?,
+    var password: String?
+)
---
+++ app/src/main/java/com/arifwidayana/challengechapter7/data/local/model/UserEntity.kt
@@ -0,0 +1,25 @@
+package com.arifwidayana.challengechapter7.data.local.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class UserEntity(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int?,
+    @ColumnInfo(name = "name_user")
+    var name: String?,
+    @ColumnInfo(name = "profile_user")
+    var profile: String?,
+    @ColumnInfo(name = "email_user")
+    var email: String?,
+    @ColumnInfo(name = "age")
+    var age: Int?,
+    @ColumnInfo(name = "phone_number_user")
+    var phone_number: String?,
+    @ColumnInfo(name = "username_user")
+    var username: String?,
+    @ColumnInfo(name = "password_user")
+    var password: String?
+)
