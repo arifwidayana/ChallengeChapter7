e0dd9e1e9202c49a5964b2c246266654cecabb8c
Arif Widayana <arifwidayana@gmail.com>
2022-05-27 08:59:23 -0700

    object constant data for any class

---
+++ app/src/main/java/com/arifwidayana/challengechapter7/utils/Constant.kt
@@ -0,0 +1,21 @@
+package com.arifwidayana.challengechapter7.utils
+
+object Constant {
+    //DatastorePreference
+    const val FINISHED = "FINISHED"
+    const val PREF = "PREF"
+    const val USERNAME = "USERNAME"
+    const val PASSWORD = "PASSWORD"
+    const val LOGIN = "LOGIN"
+
+    //Api & Endpoint
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val BASE_IMAGE = "https://image.tmdb.org/t/p/w500"
+    const val API_KEY = "554481f2801f7c87295993b4a632762a"
+    const val API_NAME = "api_key"
+    const val NOW_PLAYING = "movie/now_playing"
+    const val POPULAR = "movie/popular"
+    const val UP_COMING = "movie/upcoming"
+    const val TOP_RATED = "movie/top_rated"
+    const val DETAIL_MOVIES = "movie/{id}"
+}
