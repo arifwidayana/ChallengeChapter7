245383a370524151aa149aab848507bb23642ffd
Arif Widayana <arifwidayana@gmail.com>
2022-05-27 12:24:09 -0700

    forget to add @AndroidEntryPoint

--- app/src/main/java/com/arifwidayana/challengechapter7/ui/MainActivity.kt
+++ app/src/main/java/com/arifwidayana/challengechapter7/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.arifwidayana.challengechapter7.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.arifwidayana.challengechapter7.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
