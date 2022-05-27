cf39096aa339513ca646d9224e1dd11503338490
Arif Widayana <arifwidayana@gmail.com>
2022-05-27 08:58:20 -0700

    shared preference for relative way others datastore

---
+++ app/src/main/java/com/arifwidayana/challengechapter7/data/local/preference/SharedHelper.kt
@@ -0,0 +1,35 @@
+package com.arifwidayana.challengechapter7.data.local.preference
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.arifwidayana.challengechapter7.utils.Constant
+
+class SharedHelper(context: Context) {
+    private val sharedPref: SharedPreferences = context.getSharedPreferences(Constant.PREF, Context.MODE_PRIVATE)
+    private val editor: SharedPreferences.Editor = sharedPref.edit()
+
+    fun put(key: String, value: String) {
+        editor.putString(key, value).apply()
+    }
+
+    fun put(key: String, value: Boolean) {
+        editor.putBoolean(key, value).apply()
+    }
+
+    fun getBoolean(key: String, value: Boolean): Boolean {
+        return sharedPref.getBoolean(key, value)
+    }
+
+    fun getString(key: String, value: String): String? {
+        return sharedPref.getString(key, value)
+    }
+
+    fun getString(key: String): String? {
+        return sharedPref.getString(key, null)
+    }
+
+    fun clear() {
+        editor.clear()
+            .apply()
+    }
+}
