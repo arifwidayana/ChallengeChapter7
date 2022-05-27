10b3b51f6dabeba31041ec7312996973aa51c5eb
Arif Widayana <arifwidayana@gmail.com>
2022-05-27 10:29:47 -0700

    fix wrong code in file resource

--- app/src/main/java/com/arifwidayana/challengechapter7/base/model/Resource.kt
+++ app/src/main/java/com/arifwidayana/challengechapter7/base/model/Resource.kt
@@ -1,12 +1,10 @@
 package com.arifwidayana.challengechapter7.base.model
 
-class Resource {
-    sealed class Resource<T>(
-        val data: T? = null,
-        val message: String? = null
-    ) {
-        class Success<T>(data: T): Resource<T>(data)
-        class Loading<T>(data: T? = null): Resource<T>(data)
-        class Error<T>(data: T? = null, message: String): Resource<T>(data, message)
-    }
+sealed class Resource<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    class Success<T>(data: T): Resource<T>(data)
+    class Loading<T>(data: T? = null): Resource<T>(data)
+    class Error<T>(data: T? = null, message: String): Resource<T>(data, message)
 }
