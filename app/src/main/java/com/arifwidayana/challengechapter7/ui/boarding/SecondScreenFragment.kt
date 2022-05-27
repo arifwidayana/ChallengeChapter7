483f98b12bf9dd1ce8a799248878ae8a4f3ce7ab
Arif Widayana <arifwidayana@gmail.com>
2022-05-27 11:02:16 -0700

    on boarding using viewpager 2 with adapter for slide

---
+++ app/src/main/java/com/arifwidayana/challengechapter7/ui/boarding/SecondScreenFragment.kt
@@ -0,0 +1,36 @@
+package com.arifwidayana.challengechapter7.ui.boarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.arifwidayana.challengechapter7.R
+import com.arifwidayana.challengechapter7.databinding.FragmentSecondScreenBinding
+
+class SecondScreenFragment : Fragment() {
+    private var bind : FragmentSecondScreenBinding? = null
+    private val binding get() = bind!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        bind = FragmentSecondScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val secondScreen = activity?.findViewById<ViewPager2>(R.id.vp_on_boarding)
+        binding.tvNext.setOnClickListener {
+            secondScreen?.currentItem = 2
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        bind = null
+    }
+}
---
+++ app/src/main/java/com/arifwidayana/challengechapter7/ui/boarding/ThirdScreenFragment.kt
@@ -0,0 +1,44 @@
+package com.arifwidayana.challengechapter7.ui.boarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.arifwidayana.challengechapter7.R
+import com.arifwidayana.challengechapter7.data.local.preference.SharedHelper
+import com.arifwidayana.challengechapter7.databinding.FragmentThirdScreenBinding
+import com.arifwidayana.challengechapter7.utils.Constant
+
+class ThirdScreenFragment : Fragment() {
+    private var bind : FragmentThirdScreenBinding? = null
+    private val binding get() = bind!!
+    private lateinit var shared: SharedHelper
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        bind = FragmentThirdScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        shared = SharedHelper(requireContext())
+        binding.btnStart.setOnClickListener {
+            findNavController().navigate(R.id.action_onBoarding_to_loginFragment)
+            onBoarding()
+        }
+    }
+
+    private fun onBoarding() {
+        shared.put(Constant.FINISHED, true)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        bind = null
+    }
+}
