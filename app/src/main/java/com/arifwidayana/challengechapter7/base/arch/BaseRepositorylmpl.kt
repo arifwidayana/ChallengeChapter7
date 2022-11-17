5c2ee155a6e8a094658ac0aefc61bfcfeed0357a
Arif Widayana <arifwidayana@gmail.com>
2022-11-17 22:59:57 +0700

    deleted base view and repository impl

--- app/src/main/java/com/arifwidayana/challengechapter7/base/arch/BaseRepositorylmpl.kt
+++
@@ -1,9 +0,0 @@
-package com.arifwidayana.challengechapter7.base.arch
-
-import android.util.Log
-
-open class BaseRepositorylmpl: BaseContract.BaseRepository {
-    override fun logResponse(msg: String?) {
-        Log.d(BaseRepositorylmpl::class.java.simpleName, msg.orEmpty())
-    }
-}
