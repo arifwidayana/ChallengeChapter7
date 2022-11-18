55382d13f72a154767fd8b7a743356c27f651c62
Arif Widayana <arifwidayana@gmail.com>
2022-11-18 13:52:35 +0700

    update empty class and exception

--- app/src/main/java/com/arifwidayana/challengechapter7/base/model/Resource.kt
+++ app/src/main/java/com/arifwidayana/challengechapter7/base/model/Resource.kt
@@ -1,10 +1,18 @@
 package com.arifwidayana.challengechapter7.base.model
 
+import java.lang.Exception
+
 sealed class Resource<T>(
     val data: T? = null,
-    val message: String? = null
+    val message: String? = null,
+    val exception: Exception? = null
 ) {
-    class Success<T>(data: T): Resource<T>(data)
-    class Loading<T>(data: T? = null): Resource<T>(data)
-    class Error<T>(data: T? = null, message: String): Resource<T>(data, message)
+    class Success<T>(data: T? = null, message: String? = null) : Resource<T>(data, message)
+    class Loading<T>(data: T? = null) : Resource<T>(data)
+    class Empty<T>(data: T? = null) : Resource<T>(data)
+    class Error<T>(exception: Exception, data: T? = null, message: String? = null) : Resource<T>(
+        data = data,
+        exception = exception,
+        message = message
+    )
 }
