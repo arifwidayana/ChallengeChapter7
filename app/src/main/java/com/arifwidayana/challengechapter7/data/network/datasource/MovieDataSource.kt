9a7601d84f213556af444f1a2a3985b510bd924a
Arif Widayana <arifwidayana@gmail.com>
2022-11-18 13:50:19 +0700

    move dir and rename movie datasource

---
+++ app/src/main/java/com/arifwidayana/challengechapter7/data/network/datasource/MovieDataSource.kt
@@ -0,0 +1,38 @@
+package com.arifwidayana.challengechapter7.data.network.datasource
+
+import com.arifwidayana.challengechapter7.data.network.model.response.movie.MovieResponse
+import com.arifwidayana.challengechapter7.data.network.model.response.movie.details.DetailMovieResponse
+import com.arifwidayana.challengechapter7.data.network.service.MovieService
+import javax.inject.Inject
+
+interface MovieDataSource {
+    suspend fun getMoviesNowPlaying(): MovieResponse
+    suspend fun getMoviesPopular(): MovieResponse
+    suspend fun getMoviesUpComing(): MovieResponse
+    suspend fun getMoviesTopRated(): MovieResponse
+    suspend fun getMoviesDetail(movieId: Int): DetailMovieResponse
+}
+
+class MovieDataSourceImpl @Inject constructor(
+    private val movieService: MovieService
+): MovieDataSource {
+    override suspend fun getMoviesNowPlaying(): MovieResponse {
+        return movieService.getNowPlayingMovie()
+    }
+
+    override suspend fun getMoviesPopular(): MovieResponse {
+        return movieService.getPopularMovie()
+    }
+
+    override suspend fun getMoviesUpComing(): MovieResponse {
+        return movieService.getUpComingMovie()
+    }
+
+    override suspend fun getMoviesTopRated(): MovieResponse {
+        return movieService.getTopRatedMovie()
+    }
+
+    override suspend fun getMoviesDetail(movieId: Int): DetailMovieResponse {
+        return movieService.getDetailMovie(movieId)
+    }
+}
