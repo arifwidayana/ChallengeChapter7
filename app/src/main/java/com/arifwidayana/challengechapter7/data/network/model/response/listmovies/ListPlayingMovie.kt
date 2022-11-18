8c91116b42c1e3286bf71cc75c0d819324b4f2d6
Arif Widayana <arifwidayana@gmail.com>
2022-11-18 13:45:04 +0700

    deleted old response tmdb

--- app/src/main/java/com/arifwidayana/challengechapter7/data/network/model/response/listmovies/ListPlayingMovie.kt
+++
@@ -1,8 +0,0 @@
-package com.arifwidayana.challengechapter7.data.network.model.response.listmovies
-
-import com.google.gson.annotations.SerializedName
-
-data class ListPlayingMovie(
-    @SerializedName("results")
-    val results: List<ResultListPlaying>
-)
