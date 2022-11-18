52ca2d9842ad41f74c75624a7924b08345751dd3
Arif Widayana <arifwidayana@gmail.com>
2022-11-18 13:46:07 +0700

    delete request preference

--- app/src/main/java/com/arifwidayana/challengechapter7/data/local/model/response/UserPreference.kt
+++
@@ -1,7 +0,0 @@
-package com.arifwidayana.challengechapter7.data.local.model.response
-
-data class UserPreference(
-    val username: String,
-    val loginSession: Boolean,
-    val onBoardSession: Boolean
-)
