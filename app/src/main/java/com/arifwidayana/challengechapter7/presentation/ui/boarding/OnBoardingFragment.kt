a34ea83f6d42377855ae161b2fa11f8fdbbe1f2a
Arif Widayana <arifwidayana@gmail.com>
2022-11-24 16:40:36 +0700

    clear unused code

--- app/src/main/java/com/arifwidayana/challengechapter7/presentation/ui/boarding/OnBoardingFragment.kt
+++ app/src/main/java/com/arifwidayana/challengechapter7/presentation/ui/boarding/OnBoardingFragment.kt
@@ -3,6 +3,7 @@ package com.arifwidayana.challengechapter7.presentation.ui.boarding
 import androidx.lifecycle.ViewModel
 import com.arifwidayana.challengechapter7.base.arch.BaseFragment
 import com.arifwidayana.challengechapter7.databinding.FragmentOnBoardingBinding
+import dagger.hilt.android.AndroidEntryPoint
 
 class OnBoardingFragment : BaseFragment<FragmentOnBoardingBinding, ViewModel>(
     FragmentOnBoardingBinding::inflate
