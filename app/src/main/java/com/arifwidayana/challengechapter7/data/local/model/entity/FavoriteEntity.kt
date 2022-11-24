25ad8359c6bfd44e4f463ad501b4878d5d59ead4
Arif Widayana <arifwidayana@gmail.com>
2022-11-24 16:41:43 +0700

    add @primary key in id movie

--- app/src/main/java/com/arifwidayana/challengechapter7/data/local/model/entity/FavoriteEntity.kt
+++ app/src/main/java/com/arifwidayana/challengechapter7/data/local/model/entity/FavoriteEntity.kt
@@ -1,9 +1,11 @@
 package com.arifwidayana.challengechapter7.data.local.model.entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "favorite_table")
 data class FavoriteEntity(
+    @PrimaryKey
     var idMovie: Int,
     var username: String
 )
